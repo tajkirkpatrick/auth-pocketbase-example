e8d768ebdf383f2218e1d705d258a4a3a419bb1e
Taj K <taj.kirkpatrick@gmail.com>
2023-01-23 22:08:13 -0600

    adapter prototype installed

--- src/pages/api/auth/[...AstroAuth].ts
+++ src/pages/api/auth/[...AstroAuth].ts
@@ -1,8 +1,8 @@
 import { AstroAuth, type AstroAuthConfig } from "../../../../frameworks-astro";
 import GitHub from "@auth/core/providers/github";
-import { PocketBaseAdapter } from "../../../utils";
-import Pocketbase from "pocketbase";
+import { PocketBaseAdapter } from "../../../../adapter-pocketbase-v0.1/src";
 
+import Pocketbase from "pocketbase";
 const pb = new Pocketbase("http://127.0.0.1:8090");
 
 export const authOpts: AstroAuthConfig = {
@@ -15,6 +15,8 @@ export const authOpts: AstroAuthConfig = {
   ],
   adapter: PocketBaseAdapter(pb),
   debug: true,
+  secret: "30510956a71d8cee1816bfc65b84ed06",
+  trustHost: true,
 };
 
 export const { get, post } = AstroAuth(authOpts);
